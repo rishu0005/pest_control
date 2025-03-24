57ea245e82db48a42cbf0261800b13b81bac2ce1
rishu0005 <rishusingh5943@gmail.com>
2025-03-24 18:01:14 +0530

    make a new sections for why choose us

--- index.php
+++ index.php
@@ -148,19 +148,19 @@
                     <div class="section-title">
                         <h5 class="text-success">About Us</h5>
                         <h2 class="">Protecting home, businesses, <span class="text-green"> and communities</span></h2>
-                        <p class="text-muted mt-4 fs-5">Our mission is simple: to provide dependable pest management services that ensure a clean, safe, and comfortable environment for everyone. With years of expertise and a customer-first approach ,we go above and beyond to eliminate pests and prevent their return.</p>
+                        <p class="text-grey mt-4 fs-5">Our mission is simple: to provide dependable pest management services that ensure a clean, safe, and comfortable environment for everyone. With years of expertise and a customer-first approach ,we go above and beyond to eliminate pests and prevent their return.</p>
                     </div>
                     <!-- Section Title ENd -->
                     <div class="about-us-list d-flex align-items-center flex-warp">
                         <!-- About Us List Start -->
-                        <ul class="list-unstyled text-muted me-5 ">
+                        <ul class="list-unstyled text-grey me-5 ">
                             <li class="mb-2 "><i class="fa-solid fa-check check me-2"></i>Expertise You Can Trust</li>
                             <li class="mb-2"><i class="fa-solid fa-check check me-2"></i>Expertise You Can Trust</li>
                             <li class="mb-2"><i class="fa-solid fa-check check me-2"></i>Expertise You Can Trust</li>
                             <li class="mb-2"><i class="fa-solid fa-check check me-2"></i>Expertise You Can Trust</li>
                            
                         </ul>
-                        <ul  class="list-unstyled text-muted me-4">
+                        <ul  class="list-unstyled text-grey me-4">
                             <li class="mb-2"><i class="fa-solid fa-check check me-2"></i>Expertise You Can Trust</li>
                             <li class="mb-2"><i class="fa-solid fa-check check me-2"></i>Expertise You Can Trust</li>
                             <li class="mb-2"><i class="fa-solid fa-check check me-2"></i>Expertise You Can Trust</li>
@@ -189,23 +189,23 @@
                 <!-- Countdown Section Begin -->                         
                     <div class="border-right ">
                        <h2> <span class="counter" id="cases">0</span></h2>
-                        <p class="fs-5 text-muted">Cases Solved</p>
+                        <p class="fs-5 text-grey">Cases Solved</p>
                     </div>
 
                     <div class="border-right ">
                        <h2><span class="counter count" id="projects">0</span></h2> 
-                        <p class="fs-5 text-muted">Projects</p>
+                        <p class="fs-5 text-grey">Projects</p>
                     </div>
 
                     <div class=" border-right">
                        <h2> <span class="counter" id="clients">0</span></h2>
-                        <p class="fs-5 text-muted">Happy Clients</p>
+                        <p class="fs-5 text-grey">Happy Clients</p>
                     </div>
 
                     <div class="">
                         <h2><span  class="yearly_profit counter" id="profit">0%</span></h2>
                        
-                        <p class="fs-5 text-muted">Yearly Profit</p>
+                        <p class="fs-5 text-grey">Yearly Profit</p>
                     </div>
            
                 <!--Countdown Section End  -->
@@ -217,30 +217,234 @@
  <!-- About Us SEction ENd -->
 
  <!-- About Us Slider Start -->
-  <div class="top-paddding bg-lightsecondary">
+  <!-- <div class="top-paddding bg-lightsecondary">
 
       <div class="container ">
         <div class="row  section-row align-items-center">
             <div class="col-lg-12 mt-5">
-                <!-- Section Title start -->
+                Section Title start
                  <div class="section-title text-center">
                     <h5 class="text-white bg-success">Services</h5>
                     <h2 class="text-center ">Expert Solutions for every <span class="text-green">pest problem</span></h2>
                  </div>
     
-                <!-- Section Title ENd  -->
+                Section Title ENd 
             </div>
         </div>
 
         
       </div>
-  </div>
+  </div> -->
+
 
 
  <!-- About Us SLider ENd -->
 
  <!-- Our Features Star  -->
+<div class="top-padding">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-6">
+                <div class="section-title">
+                    <h5 class="text-green">
+                        Our Features
+                    </h5>
+                    <h2>Your trusted partner in pest <span class="text-green">control excellence</span></h2>
+                    <p class="mt-4 fs-5  text-grey">We understand how frustrating and disruptive pests can be, which is why we're committed to providing fast, reliable, and effective solutions. With a team of certified experts, eco-friendly methods, and unmatched customer care.</p>
+                </div>
 
+                <div class="our-features-list">
+                    <div class="d-flex flex-wrap">
+                        <div class="features-item d-flex">
+                            <div class="icon-box">
+                                <img src="https://html.awaikenthemes.com/petronus/images/icon-features-item-1.svg" alt="">
+                            </div>
+                            <div class="feature-items-content">
+                                <h5 class="fw-7">Emergency Services</h5>
+                                <p class="text-grey word-wrap w-75">We are available round the clock to address.</p>
+
+                            </div>
+                        </div>
+
+                        <div class="features-item d-flex">
+                            <div class="icon-box">
+                                <img src="https://html.awaikenthemes.com/petronus/images/icon-features-item-1.svg" alt="">
+                            </div>
+                            <div class="feature-items-content">
+                                <h5 class="fw-7">Emergency Services</h5>
+                                <p class="text-grey word-wrap w-75">We are available round the clock to address.</p>
+
+                            </div>
+                        </div>
+
+                        <div class="features-item d-flex">
+                            <div class="icon-box">
+                                <img src="https://html.awaikenthemes.com/petronus/images/icon-features-item-1.svg" alt="">
+                            </div>
+                            <div class="feature-items-content">
+                                <h5 class="fw-7">Emergency Services</h5>
+                                <p class="text-grey word-wrap w-75">We are available round the clock to address.</p>
+
+                            </div>
+                        </div>
+
+                        <div class="features-item d-flex">
+                            <div class="icon-box">
+                                <img src="https://html.awaikenthemes.com/petronus/images/icon-features-item-1.svg" alt="">
+                            </div>
+                            <div class="feature-items-content">
+                                <h5 class="fw-7">Emergency Services</h5>
+                                <p class="text-grey word-wrap w-75">We are available round the clock to address.</p>
+
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+            <div class="col-lg-6">
+                <div class="features-image">
+               
+                    <figure class="image-anime reveal" style="transform: translate(0px, 0px); opacity: 1; visibility: inherit;">
+                            <!-- <img src="images/features-image.jpg" alt="" style="transform: translate(0px, 0px);"> -->
+                        
+                        <img src="https://html.awaikenthemes.com/petronus/images/features-image.jpg" alt="heelo why are you not coming" alt="" style="transform: translate(0px, 0px);">
+                    </figure>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Our Features ENd -->
+
+<!-- Portfoilo Start -->
+ <!-- <div class="top-padding bg-lightsecondary">
+    <div class="container">
+        <div class="row section-row align-items-center">
+            <div class="col-lg-12">
+                <div class="section-title section-title-center">
+                    <h5 class="bg-green text-white">Our Portfoilo</h5>
+                    <h2 class="">Explore our proven track record of <span class="text-green">pest-free Solutions</span> </h2>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-lg-12 col-md-12">
+                <div class="our-project-nav">
+
+                    <ul class="list-unstyled flex-warp d-flex align-items-center mt-4 justify-content-center g-5">
+                    <li><a href="" class=" text-decoration-none text-grey me-4">All</a> </li>
+                    <li><a href="" class="  text-decoration-none text-grey me-4">  Home Pest</a></li>
+                    <li><a href="" class="text-decoration-none text-grey me-4"> Commericial Pest</a></li>
+                    <li><a href="" class="text-decoration-none text-grey me-4"> Eco-Friendly Pest</a> </li>
+                    <li><a href="" class="text-decoration-none text-grey me-4"> Termite & Rodent</a> </li>
+                    <li><a href="" class="text-decoration-none text-grey me-4">Outdoor Pest</a> </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+
+    </div>
+ </div> -->
+
+ <!-- Portfolio ENd -->
+
+ <!-- WHy Choose Us Start -->
+ <div class="top-padding bg-lightsecondary container-fluid">
+ <div class="container">
+                
+     <div class="row  section-row align-items-center">
+        
+         <div class="col-lg-12">
+             <div class=" section-title section-title-center">
+                 <h5 class="bg-green text-white">Why Choose Us</h5>
+                 <h2 class="">What makes us the best <span class="text-green">choice for you</span> </h2>
+                 <p class="text-grey mt-3">We understand how frustrating and disruptive pests can be, which is why we're committed to providing fast, reliable, and effective solutions.</p>
+             </div>
+         </div>
+     </div>
+
+     <div class="row">
+         <div class="col-lg-12 m-5">
+            <div class="why-choose-us-box gap-4 d-flex flex-wrap">
+                <div class="why-choose-us-item">
+                    <div class="icon-box">
+                        <img src="https://html.awaikenthemes.com/petronus/images/icon-why-choose-1.svg" alt="">
+                    </div>
+                    <div class="why-choose-item-content">
+                        <h3 class="fw-7">Emergency Services</h3>
+                        <p class="text-grey">We are available round the clock to address</p>
+                    </div>
+
+
+                </div>
+                <div class="why-choose-us-item">
+                <div class="icon-box">
+                        <img src="https://html.awaikenthemes.com/petronus/images/icon-why-choose-1.svg" alt="">
+                    </div>
+                    <div class="why-choose-item-content">
+                        <h3 class="fw-7">Customized Treatments</h3>
+                        <p class="text-grey">We are available round the clock to address</p>
+                    </div>
+                </div>
+                <div class="why-choose-us-item">
+                <div class="icon-box">
+                        <img src="https://html.awaikenthemes.com/petronus/images/icon-why-choose-1.svg" alt="">
+                    </div>
+                    <div class="why-choose-item-content">
+                        <h3 class="fw-7">Certified Professionals</h3>
+                        <p class="text-grey">We are available round the clock to address</p>
+                    </div>
+                </div>
+                <div class="page-cta-box">
+                <div class="page-cta-box-info">
+                    <h3 class="text-white fw-7 fs-5 mb-5">Take Back Your Home From Pests. Today!</h3>
+                    <div class="cta-contact-info">
+                        <p class="text-white fw-7"> CALL NOW </p>
+                        <div class="cta-contact-item d-flex  align-items-center">
+                            <div class="icon-box">
+                                <img src="https://html.awaikenthemes.com/petronus/images/icon-cta-phone.svg" alt="">
+                                
+                            </div>
+                            <div class="cta-contact-item-content text-white">
+                                <h3 class="text-white">+1 322-782-968</h3>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                </div>
+            </div>
+             
+         </div>
+         <div class="col-lg-12 mt-5 text-center">
+            <p class="text-grey"> <span class="btn bg-green text-white">Free</span> Let's create a pest-free environment - <a href="" class="text-green">Contact Us Today</a> </p>
+         </div>
+     </div>
+                </div>
+
+
+ </div>
+ <!-- Why Choose Us End -->
+
+ <!-- FAQ Start -->
+<div class="container-fluid bg-navyblue top-padding">
+    <div class="container">
+        <div class="row  align-items-center">
+            <div class="col-lg-6">
+                <div class="our-faq-images">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+ <!-- FAQ End -->
  
 
 <?php include 'footer.php'; ?>
